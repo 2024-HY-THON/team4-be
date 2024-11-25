4bbde201439562116769e8ce2932abb95a80e29b
msk226 <kminseok5167@gmail.com>
2024-11-25 21:40:44 +0900

    [chore] Swagger 의존성 추가 및 적용

--- src/main/java/com/example/hython/controller/TestController.java
+++ src/main/java/com/example/hython/controller/TestController.java
@@ -1,14 +1,19 @@
 package com.example.hython.controller;
 
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/")
+@CrossOrigin
+@Tag(name = "Test Controller", description = "Test Controller")
 @RestController
 public class TestController {
 
-    @RequestMapping("/health")
+    @GetMapping("/health")
     public String healthCheck() {
         return "Hello, World!";
     }
