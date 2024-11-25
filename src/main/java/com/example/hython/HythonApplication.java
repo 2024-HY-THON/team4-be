e8760a1400959d17f12378470b5c7e54fd359556
msk226 <kminseok5167@gmail.com>
2024-11-25 14:28:41 +0900

    [chore] 초기 프로젝트 파일 업로드

---
+++ src/main/java/com/example/hython/HythonApplication.java
@@ -0,0 +1,13 @@
+package com.example.hython;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HythonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HythonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hython/HythonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hython;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HythonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
