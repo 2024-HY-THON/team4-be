484ec9a0df229fe3deb2c43272f566e0bffe6210
msk226 <kminseok5167@gmail.com>
2024-11-28 00:20:34 +0900

    [chore] CORS 해결 위해 애노테이션 추가

--- src/main/java/com/example/hython/domain/recipe/RecipeController.java
+++ src/main/java/com/example/hython/domain/recipe/RecipeController.java
@@ -5,6 +5,7 @@ import com.example.hython.domain.member.dtos.MemberRequestDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/sum")
 @RequiredArgsConstructor
 public class RecipeController {
