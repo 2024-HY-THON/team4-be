86ae3760ab5c725e6c4db492c7043075e016ae1b
msk226 <kminseok5167@gmail.com>
2024-11-29 20:00:27 +0900

    [feat] 알림 스케쥴링 기능 구현

--- src/main/java/com/example/hython/domain/alarm/FcmMessageService.java
+++ src/main/java/com/example/hython/domain/alarm/FcmMessageService.java
@@ -2,12 +2,20 @@ package com.example.hython.domain.alarm;
 
 import com.example.hython.domain.member.MemberService;
 import com.example.hython.domain.member.utils.JWTUtils;
+import com.example.hython.domain.repose.Repose;
+import com.example.hython.domain.repose.ReposeRepository;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -15,6 +23,7 @@ import org.springframework.stereotype.Service;
 public class FcmMessageService {
 
     private final MemberService memberService;
+    private final ReposeRepository reposeRepository;
     private final JWTUtils jwtUtils;
 
     public String sendMessage(FcmMessageRequestDTO requestDto) {
@@ -36,6 +45,42 @@ public class FcmMessageService {
         } catch (FirebaseMessagingException e) {
             e.printStackTrace();
             return "Failed to send message";
+        }
+    }
+    @Transactional
+    @Scheduled(cron = "0 * * * * *") // 매 분마다 실행
+    public void sendReposeAlarm() {
+        // 현재 시간
+        LocalTime now = LocalTime.now();
+        log.info(now.toString());
+        // 현재 시간에서 5분 뒤
+        LocalTime fiveMinutesBefore = now.plusMinutes(5);
+
+        // 5분 전에 시작하는 휴식 조회
+        List<Repose> reposeList = reposeRepository.findByStartTimeBetween(now, fiveMinutesBefore);
+
+        for (Repose repose : reposeList) {
+            // 사용자의 Firebase 토큰 값을 조회
+            String userFirebaseToken = memberService.findFirebaseTokenByMemberId(repose.getMember().getId());
+
+            // 메시지 구성
+            Message message = Message.builder()
+                    .putData("title", "일상 속 짧은 해방의 순간, 숨")
+                    .putData("content", repose.getMinutes() + " 동안 " + repose.getRecipe().getRecipe() + "으로 휴식을 갖는건 어떠신가요?")
+                    .setToken(userFirebaseToken) // 조회한 토큰 값을 사용
+                    .build();
+
+            log.info("message is "  + message.toString());
+
+            try {
+                // 메시지 전송
+                String response = FirebaseMessaging.getInstance().send(message);
+                System.out.println("Message sent successfully: " + response); // 성공 로그 출력
+            } catch (FirebaseMessagingException e) {
+                // 예외 처리
+                e.printStackTrace();
+                System.out.println("Failed to send message");
+            }
         }
     }
 }
--- src/main/java/com/example/hython/domain/repose/ReposeRepository.java
+++ src/main/java/com/example/hython/domain/repose/ReposeRepository.java
@@ -3,6 +3,8 @@ package com.example.hython.domain.repose;
 import com.example.hython.domain.member.Member;
 import com.example.hython.domain.recipe.Recipe;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -13,4 +15,5 @@ public interface ReposeRepository extends JpaRepository<Repose, Long> {
     boolean existsByMemberAndRecipe(Member member, Recipe recipe);
     List<Repose> findByMemberAndIsDone(Member member, boolean isDone);
     List<Repose> findByMemberAndStartDateBetweenAndIsDone(Member member, LocalDate startDate, LocalDate endDate, boolean isDone);
+    List<Repose> findByStartTimeBetween(LocalTime startDate, LocalTime endDate);
 }
